ad8275f3242896e7a15c00f42a239dce099c47fb
jean.dossantos <jeandsantos88@gmail.com>
2024-02-15 22:33:16 +0100

    Configured loggers

--- src/workbench_core/logging/logging_configs.py
+++ src/workbench_core/logging/logging_configs.py
@@ -27,69 +27,6 @@ LOG_LEVEL_MAP: dict[EnvState, int] = {
     EnvState.PROD: logging.INFO,
 }
 
-LOGGING_CONFIG = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "formatters": {
-        "detailed": {
-            "class": "logging.Formatter",
-            "format": "[%(levelname)-8s|%(module)s|L%(lineno)d] %(asctime)s: %(name)-15s %(message)s",
-            "datefmt": "%Y-%m-%d %H:%M:%S%z",
-        },
-        "simple": {
-            "class": "logging.Formatter",
-            "format": "%(asctime)s %(levelname)-8s %(message)s",
-            "datefmt": "%Y-%m-%d %H:%M:%S%z",
-        },
-        "json": {
-            "()": "src.workbench_core.logging.formatters.json_formatter.JSONFormatter",
-            "fmt_keys": {
-                "level": "levelname",
-                "message": "message",
-                "timestamp": "timestamp",
-                "logger": "logger",
-                "module": "module",
-                "function": "funcName",
-                "line": "lineno",
-                "thread_name": "threadName",
-            },
-        },
-    },
-    "handlers": {
-        "streaming_handler": {
-            "class": "rich.logging.RichHandler",
-            "level": LOG_LEVEL_MAP[ENV_STATE],
-            "formatter": "console_formatter",
-            "omit_repeated_times": False,
-            "show_level": True,
-            "show_path": False,
-            "enable_link_path": True,
-            "rich_tracebacks": True,
-        },
-        "file_handler": {
-            "class": "logging.handlers.RotatingFileHandler",
-            "level": LOG_LEVEL_MAP[ENV_STATE],
-            "formatter": "detailed",
-            "filename": LOG_FILEPATH,
-            "maxBytes": 1024 * 1024 * 10,
-            "backupCount": 3,
-            "encoding": "utf8",
-        },
-    },
-    "loggers": {
-        "": {
-            "handlers": ["streaming_handler", "file_handler"],
-            "level": LOG_LEVEL_MAP[ENV_STATE],
-            "propagate": False,
-        },
-        "__main__": {
-            "handlers": ["streaming_handler", "file_handler"],
-            "level": LOG_LEVEL_MAP[ENV_STATE],
-            "propagate": False,
-        },
-    },
-}
-
 
 def get_handler_by_name(logger: logging.Logger, handler_name: str):
     for handler in logger.handlers:
