a6d08d203b8eabd987087016aa2da12ff62ee257
jean.dossantos <jeandsantos88@gmail.com>
2024-02-18 16:27:16 +0100

    Renamed test files for transformers and data classes

--- src/workbench_core/config/data_object_factory.py
+++
@@ -1,8 +0,0 @@
-from workbench_core.workbench_data.workbench_data import WorkbenchData
-from workbench_core.workbench_factory import WorkbenchFactory
-
-
-class WorkbenchDataFactory(WorkbenchFactory):
-    """Factory for WorkbenchData classes."""
-
-    _items: dict[str, WorkbenchData] | None = None
