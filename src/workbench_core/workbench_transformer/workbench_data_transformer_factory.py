7f4b1dc6380094bfcacfde9bc38ec7fc26693ad1
jean.dossantos <jeandsantos88@gmail.com>
2024-02-18 16:21:05 +0100

    Delete redundant data transformer factory classes

--- src/workbench_core/workbench_transformer/workbench_data_transformer_factory.py
+++
@@ -1,8 +0,0 @@
-from workbench_core.workbench_factory import WorkbenchFactory
-from workbench_core.workbench_transformer.workbench_transfomer import WorkbenchTransformer
-
-
-class WorkbenchTransformerFactory(WorkbenchFactory):
-    """Factory for WorkbenchTransformer classes."""
-
-    _items: dict[str, WorkbenchTransformer] | None = None
