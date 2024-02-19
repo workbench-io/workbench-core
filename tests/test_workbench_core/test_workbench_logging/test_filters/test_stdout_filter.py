51e9f56418303c0b79b9f4a2d695b9dd6e6a7707
jean.dossantos <jeandsantos88@gmail.com>
2024-02-19 22:05:23 +0100

    Add tests for stderr and stdout filters

---
+++ tests/test_workbench_core/test_workbench_logging/test_filters/test_stdout_filter.py
@@ -0,0 +1,18 @@
+import logging
+
+from workbench_core.workbench_logging.filters.stdout_filter import StdoutFilter
+
+
+class TestStdoutFilter:
+
+    def test_filter_returns_false_when_levelno_is_greater_than_info(self, log_record_warning: logging.LogRecord):
+        filter_obj = StdoutFilter()
+        result = filter_obj.filter(log_record_warning)
+
+        assert result is False
+
+    def test_filter_returns_true_when_levelno_is_info_or_lower(self, log_record_info: logging.LogRecord):
+        filter_obj = StdoutFilter()
+        result = filter_obj.filter(log_record_info)
+
+        assert result is True
