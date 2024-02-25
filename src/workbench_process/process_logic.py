b50a4603f81936470d73252769c0da5b6515c28d
jean.dossantos <jeandsantos88@gmail.com>
2024-02-25 20:15:51 +0100

    Add RenameColumns to ProcessLogic

--- src/workbench_process/process_logic.py
+++ src/workbench_process/process_logic.py
@@ -4,6 +4,7 @@ from workbench_process.common import Source
 from workbench_process.process_data import ProcessData
 from workbench_process.process_settings import ProcessSettings
 from workbench_process.sources.source_factory import factory_source
+from workbench_process.transformers.rename_columns import RenameColumns
 
 
 class ProcessLogic(WorkbenchLogic):
@@ -16,6 +17,7 @@ class ProcessLogic(WorkbenchLogic):
         self.log_info(self.run, "Running process")
 
         self._load_sources(data, settings)
+        self._perform_transformations(data, settings)
 
         self.log_info(self.run, "Process complete")
 
@@ -28,3 +30,11 @@ class ProcessLogic(WorkbenchLogic):
             source: WorkbenchSource = factory_source.create_instance(name=source)
             source.load(data, settings)
             self.log_info(self.run, f"Source {source} processed")
+
+    def _perform_transformations(self, data: ProcessData, settings: ProcessSettings):
+
+        self.log_info(self._perform_transformations, "Performing transformations")
+
+        RenameColumns().transform(data, settings)
+
+        self.log_info(self._perform_transformations, "Transformations complete")
