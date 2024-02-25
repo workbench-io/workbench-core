c91f8d8f23a3e33c6d0f4536358729583adaf971
jean.dossantos <jeandsantos88@gmail.com>
2024-02-25 20:10:34 +0100

    Add RenameColumns transformer and corresponding tests

---
+++ src/workbench_process/transformers/rename_columns.py
@@ -0,0 +1,27 @@
+from workbench_components.workbench_data.workbench_data import WorkbenchData
+from workbench_components.workbench_settings.workbench_settings import WorkbenchSettings
+from workbench_components.workbench_transformer.workbench_transformer import WorkbenchTransformer
+from workbench_process.common import Source
+
+
+class RenameColumns(WorkbenchTransformer):
+    """Rename columns in a DataFrame."""
+
+    def transform(self, data: WorkbenchData, settings: WorkbenchSettings) -> bool:  # pylint: disable=unused-argument
+        """Renames columns in a DataFrame."""
+
+        for source in Source:
+            self.log_debug(self.transform, f"Renaming columns for {source.value}")
+
+            source_data = getattr(settings.model.sources, source.value, None)
+            if source_data is None:
+                self.log_debug(self.transform, f"No column map found for {source.value}")
+                continue
+
+            column_mapping = source_data.columns
+            self.log_debug(self.transform, f"Using column map:{column_mapping}")
+
+            data_renamed = data.get_data(source).rename(columns=column_mapping)
+            data.set_data(source, data_renamed)
+
+        return True
---
+++ tests/test_workbench_process/test_transformers/test_rename_columns.py
@@ -0,0 +1,35 @@
+import pandas as pd
+
+from workbench_process.process_data import ProcessData
+from workbench_process.process_settings import ProcessSettings
+from workbench_process.transformers.rename_columns import RenameColumns
+
+
+class TestRenameColumns:
+    def test_transform_returns_true(
+        self,
+        concrete_compressive_strength_raw: pd.DataFrame,
+        process_data: ProcessData,
+        process_settings: ProcessSettings,
+    ):
+
+        process_data.compressive_strength = concrete_compressive_strength_raw
+        result = RenameColumns().transform(process_data, process_settings)
+
+        assert result is True
+
+    def test_transform_changes_the_column_names(
+        self,
+        concrete_compressive_strength_raw: pd.DataFrame,
+        concrete_compressive_strength_after_name_change: pd.DataFrame,
+        process_data: ProcessData,
+        process_settings: ProcessSettings,
+    ):
+
+        process_data.compressive_strength = concrete_compressive_strength_raw
+        RenameColumns().transform(process_data, process_settings)
+
+        assert (
+            process_data.compressive_strength.columns == concrete_compressive_strength_after_name_change.columns
+        ).all()
+        assert process_data.compressive_strength.equals(concrete_compressive_strength_after_name_change)
