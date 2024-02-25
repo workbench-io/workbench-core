9c0a0e1335105defa182525e6145ed91bfdec932
jean.dossantos <jeandsantos88@gmail.com>
2024-02-25 18:23:10 +0100

    Removed tox from pre-commit

--- src/workbench_process/process_logic.py
+++ src/workbench_process/process_logic.py
@@ -1,6 +1,9 @@
 from workbench_components.workbench_logic.workbench_logic import WorkbenchLogic
+from workbench_components.workbench_source.workbench_source import WorkbenchSource
+from workbench_process.common import Source
 from workbench_process.process_data import ProcessData
 from workbench_process.process_settings import ProcessSettings
+from workbench_process.sources.source_factory import factory_source
 
 
 class ProcessLogic(WorkbenchLogic):
@@ -9,6 +12,19 @@ class ProcessLogic(WorkbenchLogic):
         self,
         data: ProcessData,
         settings: ProcessSettings,
-    ):  # pylint: disable=unused-argument
+    ):
         self.log_info(self.run, "Running process")
+
+        self._load_sources(data, settings)
+
+        self.log_info(self.run, "Process complete")
+
         return True
+
+    def _load_sources(self, data: ProcessData, settings: ProcessSettings):
+
+        for source in Source:
+            self.log_info(self.run, f"Processing source: {source}")
+            source: WorkbenchSource = factory_source.create_instance(name=source)
+            source.load(data, settings)
+            self.log_info(self.run, f"Source {source} processed")
