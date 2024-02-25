70a34e3f2c4b6a1d0fca4faa2d8949c9c27b397e
jean.dossantos <jeandsantos88@gmail.com>
2024-02-25 17:04:26 +0100

    Add main.py file;Fix paths in logging_config.json

---
+++ main.py
@@ -0,0 +1,19 @@
+from workbench_components.workbench_logging.logging_configs import setup_logging
+from workbench_process.common import STEP_PROCESS
+from workbench_process.process_factory import factory_process
+
+
+def run_process() -> None:
+    """Run the loading and processing of the data."""
+    process_logic, process_data, process_settings = factory_process.create_instance(name=STEP_PROCESS)
+    process_logic.run(data=process_data, config=process_settings)
+
+
+def main() -> None:
+
+    setup_logging()
+    run_process()
+
+
+if __name__ == "__main__":
+    main()
