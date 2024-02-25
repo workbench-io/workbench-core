b8d0590685126f6a73010c0fba3d3a382ca4a4e1
jean.dossantos <jeandsantos88@gmail.com>
2024-02-25 19:05:57 +0100

    Add test for adding sources to ProcessData object

--- tests/test_workbench_process/test_process_logic.py
+++ tests/test_workbench_process/test_process_logic.py
@@ -1,3 +1,6 @@
+import pandas as pd
+
+from workbench_process.common import Source
 from workbench_process.process_data import ProcessData
 from workbench_process.process_logic import ProcessLogic
 from workbench_process.process_settings import ProcessSettings
@@ -5,9 +8,28 @@ from workbench_process.process_settings import ProcessSettings
 
 class TestProcess:
 
-    def test_run_should_return_true(self, process_data: ProcessData, process_settings: ProcessSettings):
+    def test_run_should_return_true(
+        self,
+        process_data: ProcessData,
+        process_settings: ProcessSettings,
+    ):
         process = ProcessLogic()
         result = process.run(process_data, process_settings)
 
         assert isinstance(result, bool)
         assert result
+
+    def test_run_should_add_sources_to_process_data_object(
+        self,
+        process_data: ProcessData,
+        process_settings: ProcessSettings,
+    ):
+        process = ProcessLogic()
+        process.run(process_data, process_settings)
+
+        for source in Source:
+            source_data = process_data.get_data(source)
+
+            assert source_data is not None
+            assert not source_data.empty
+            assert isinstance(source_data, pd.DataFrame)
