66aa4e1af49e04afee0e6fa83ac0b229595fa183
jean.dossantos <jeandsantos88@gmail.com>
2024-03-06 15:03:47 +0100

    Implemented logic and tests for SplitTrainTestSet

---
+++ src/workbench_train/transformers/split_train_test_set.py
@@ -0,0 +1,33 @@
+from sklearn.model_selection import train_test_split
+
+from workbench_components.workbench_transformer.workbench_transformer import WorkbenchTransformer
+from workbench_train.train_data import TrainData
+from workbench_train.train_settings import TrainSettings
+
+
+class SplitTrainTestSet(WorkbenchTransformer):
+    """Split data into training and test sets."""
+
+    def transform(self, data: TrainData, settings: TrainSettings) -> bool:
+        """Split data into training and test sets."""
+
+        self.log_debug(self.transform, "Spliting Data")
+
+        x_train, x_test, y_train, y_test = train_test_split(
+            data.features,
+            data.targets,
+            test_size=settings.model.test_size,
+            random_state=settings.model.seed,
+        )
+
+        data.x_train = x_train
+        data.x_test = x_test
+        data.y_train = y_train
+        data.y_test = y_test
+
+        self.log_debug(
+            self.transform,
+            f"Data spliting completed: train set size: {len(data.x_train)}; test set size: {len(data.x_test)}",
+        )
+
+        return True
---
+++ tests/test_workbench_train/test_transformers/test_split_train_test_set.py
@@ -0,0 +1,39 @@
+import pandas as pd
+
+from workbench_train.train_data import TrainData
+from workbench_train.train_settings import TrainSettings
+from workbench_train.transformers.split_train_test_set import SplitTrainTestSet
+
+
+class TestSplitData:
+    def test_transform_returns_true(
+        self,
+        train_data: TrainData,
+        train_settings: TrainSettings,
+    ):
+
+        train_data.features = pd.DataFrame({"A": range(0, 10)})
+        train_data.targets = pd.DataFrame({"B": range(10, 20)})
+        train_settings.model.test_size = 0.2
+
+        result = SplitTrainTestSet().transform(train_data, train_settings)
+
+        assert result is True
+
+    def test_transform_create_train_and_test_sets_of_expected_proportions(
+        self,
+        train_data: TrainData,
+        train_settings: TrainSettings,
+    ):
+
+        train_data.features = pd.DataFrame({"A": range(0, 10)})
+        train_data.targets = pd.DataFrame({"B": range(10, 20)})
+        train_settings.model.test_size = 0.2
+
+        SplitTrainTestSet().transform(train_data, train_settings)
+
+        assert len(train_data.x_train) == 8
+        assert len(train_data.y_train) == 8
+
+        assert len(train_data.x_test) == 2
+        assert len(train_data.y_test) == 2
