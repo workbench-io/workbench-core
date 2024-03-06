3c5acaf96ec23f810d21e9695ff708878942bfab
jean.dossantos <jeandsantos88@gmail.com>
2024-03-06 11:48:44 +0100

    Created TrainData.from_process_data() method

--- src/workbench_train/train_data.py
+++ src/workbench_train/train_data.py
@@ -3,12 +3,14 @@ from dataclasses import dataclass, field
 import pandas as pd
 
 from workbench_components.workbench_data.workbench_data import WorkbenchData
+from workbench_process.process_data import ProcessData
 from workbench_utils.utils_dataframes import generate_dataframe
 
 
 @dataclass
 class TrainData(WorkbenchData):
 
+    processed: pd.DataFrame = field(default_factory=generate_dataframe)
     x_train: pd.DataFrame = field(default_factory=generate_dataframe)
     x_test: pd.DataFrame = field(default_factory=generate_dataframe)
     y_train: pd.DataFrame = field(default_factory=generate_dataframe)
@@ -16,3 +18,6 @@ class TrainData(WorkbenchData):
 
     def __post_init__(self):
         super().__init__()
+
+    def from_process_data(self, data: ProcessData) -> None:
+        self.processed = data.compressive_strength
--- tests/test_workbench_train/test_train_data.py
+++ tests/test_workbench_train/test_train_data.py
@@ -1,3 +1,7 @@
+import pandas as pd
+from pandas.testing import assert_frame_equal
+
+from workbench_process.process_data import ProcessData
 from workbench_train.train_data import TrainData
 
 
@@ -7,3 +11,13 @@ class TestTrainData:
         train_data = TrainData()
 
         assert isinstance(train_data, TrainData)
+
+    def test_train_data_from_process_data_loads_data_from_process_data_object(self):
+
+        process_data = ProcessData()
+        process_data.compressive_strength = pd.DataFrame({"A": [1, 2, 3]})
+
+        train_data = TrainData()
+        train_data.from_process_data(process_data)
+
+        assert_frame_equal(train_data.processed, pd.DataFrame({"A": [1, 2, 3]}))
