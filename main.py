89e7d229eff36e92a4c728168ae586cdb392cb44
jean.dossantos <jeandsantos88@gmail.com>
2024-03-07 12:11:58 +0100

    changes to main.py

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ from workbench_train.train_factory import factory_train
 from workbench_utils.common import WorkbenchSteps
 
 
-def run_process() -> None:
-    """Run the loading and processing of the data."""
+def run_all_logic() -> None:
+    """Run all the logic"""
+
     process_logic, process_data, process_settings = factory_process.create_instance(name=WorkbenchSteps.PROCESS)
     process_settings.load_settings_from_file("tests/test_workbench_process/resources/workbench_settings.json")
     process_logic.run(process_data, process_settings)
@@ -13,14 +14,13 @@ def run_process() -> None:
     train_logic, train_data, train_settings = factory_train.create_instance(name=WorkbenchSteps.TRAIN)
     train_settings.load_settings_from_file("tests/test_workbench_train/resources/workbench_settings.json")
     train_data.from_process_data(process_data)
-
     train_logic.run(train_data, train_settings)
 
 
 def main() -> None:
 
     setup_logging()
-    run_process()
+    run_all_logic()
 
 
 if __name__ == "__main__":
