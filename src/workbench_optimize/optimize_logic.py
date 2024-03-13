21095b82a154c1a347206fe8df147fa52a5ca832
jean.dossantos <jeandsantos88@gmail.com>
2024-03-13 18:22:49 +0200

    Refactor optimize_logic.py to use OptimizeData and OptimizeSettings

--- src/workbench_optimize/optimize_logic.py
+++ src/workbench_optimize/optimize_logic.py
@@ -1,15 +1,15 @@
 from workbench_components.workbench_logic.workbench_logic import WorkbenchLogic
+from workbench_optimize.optimize_data import OptimizeData
+from workbench_optimize.optimize_settings import OptimizeSettings
 from workbench_optimize.transformers.run_optimization import RunOptimization
-from workbench_train.train_data import TrainData
-from workbench_train.train_settings import TrainSettings
 
 
 class OptimizeLogic(WorkbenchLogic):
 
     def run(
         self,
-        data: TrainData,
-        settings: TrainSettings,
+        data: OptimizeData,
+        settings: OptimizeSettings,
     ):
         self.log_info(self.run, "Running optimization")
         RunOptimization().transform(data, settings)
