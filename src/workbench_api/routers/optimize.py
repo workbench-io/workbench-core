e2bb136fb5a5228bbd010d0722b4c1715c3b3c87
jean.dossantos <jeandsantos88@gmail.com>
2024-03-16 11:22:18 +0200

    Refactor optimize router and load estimator from directory

--- src/workbench_api/routers/optimize.py
+++ src/workbench_api/routers/optimize.py
@@ -7,26 +7,29 @@ from workbench_api.models.optimize import OptimizeOutputModel
 from workbench_optimize.optimize_factory import factory_optimize
 from workbench_train.common import Targets
 from workbench_utils.enums import WorkbenchSteps
+from workbench_utils.export import load_estimator_from_directory
 
 router = APIRouter()
 
 logger = logging.getLogger(__name__)
 
 
+PATH_OPTIMIZE_SETTINGS = (
+    "/Users/jean/learn/workbench/workbench-core/tests/test_workbench_optimize/resources/workbench_settings.json"
+)
+
+
 @router.get("/optimize", status_code=status.HTTP_200_OK, response_model=OptimizeOutputModel)
 async def run_optimization():
 
-    optimize_logic, optimize_data, optimize_settings = factory_optimize.create_instance(name=WorkbenchSteps.OPTIMIZE)
-    optimize_settings.load_settings_from_file(
-        pathlib.Path(
-            "/Users/jean/learn/workbench/workbench-core/tests/test_workbench_optimize/resources/workbench_settings.json"
-        )
-    )
+    logic, data, settings = factory_optimize.create_instance(name=WorkbenchSteps.OPTIMIZE)
 
-    logger.debug(
-        f"Making optimization for '{Targets.COMPRESSIVE_STRENGTH}' with input: {optimize_settings.model.model_dump()}"
-    )
-    optimize_logic.run(optimize_data, optimize_settings)
-    logger.debug(f"Predicted value for '{Targets.COMPRESSIVE_STRENGTH}': {optimize_data.results}")
+    settings.load_settings_from_file(pathlib.Path(PATH_OPTIMIZE_SETTINGS))
 
-    return OptimizeOutputModel(**optimize_data.results.model_dump())
+    data.model = load_estimator_from_directory(settings.model.path_model)
+
+    logger.debug(f"Making optimization for '{Targets.COMPRESSIVE_STRENGTH}' with input: {settings.model.model_dump()}")
+    logic.run(data, settings)
+    logger.debug(f"Predicted value for '{Targets.COMPRESSIVE_STRENGTH}': {data.results}")
+
+    return OptimizeOutputModel(**data.results.model_dump())
