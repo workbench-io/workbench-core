936e4c4125669dd7d694a05892defca9522feea0
jean.dossantos <jeandsantos88@gmail.com>
2024-03-20 14:06:40 +0200

    Use repository creation in optimize router

--- src/workbench_api/routers/optimize.py
+++ src/workbench_api/routers/optimize.py
@@ -4,9 +4,9 @@ from typing import Annotated
 from fastapi import APIRouter, Body, Depends, HTTPException, Response, status
 from sklearn.base import BaseEstimator
 
-from workbench_api.data import db
+from workbench_api.data.repository import ListRepository, get_optimizations_repository
 from workbench_api.models.optimize import OptimizeInputModel, OptimizeOutputModel
-from workbench_api.utils import get_db_entry_by_id, get_model, get_next_id
+from workbench_api.utils import get_model
 from workbench_optimize.optimize_factory import factory_optimize
 from workbench_train.common import Targets
 from workbench_utils.enums import WorkbenchSteps
@@ -19,7 +19,8 @@ logger = logging.getLogger(__name__)
 @router.post("/optimize", status_code=status.HTTP_201_CREATED, response_model=OptimizeOutputModel)
 async def run_optimization(
     optimization_input: Annotated[OptimizeInputModel, Body(...)],
-    model: BaseEstimator = Depends(get_model),
+    model: Annotated[BaseEstimator, Depends(get_model)],
+    repo: Annotated[ListRepository, Depends(get_optimizations_repository)],
 ) -> OptimizeOutputModel:
 
     logic, data, settings = factory_optimize.create_instance(name=WorkbenchSteps.OPTIMIZE)
@@ -32,7 +33,7 @@ async def run_optimization(
     logic.run(data, settings)
     logger.debug(f"Predicted value for '{Targets.COMPRESSIVE_STRENGTH}': {data.results}")
 
-    db_id = get_next_id(db.optimizations)
+    db_id = repo.get_next_id()
 
     result = OptimizeOutputModel(
         id=db_id,
@@ -41,32 +42,39 @@ async def run_optimization(
     )
 
     logger.debug(f"Saving result to database {result} with id {db_id}")
-    db.optimizations.append(result)
+    repo.add(db_id, result)
 
     return result
 
 
 @router.get("/optimize", status_code=status.HTTP_200_OK)
-async def get_all_optimizations() -> list[OptimizeOutputModel]:
-    return db.optimizations
+async def get_all_optimizations(
+    repo: Annotated[ListRepository, Depends(get_optimizations_repository)],
+) -> list[OptimizeOutputModel]:
+    return repo.get_all()
 
 
 @router.get("/optimize/latest", status_code=status.HTTP_200_OK, response_model=OptimizeOutputModel)
-async def get_last_optimization() -> OptimizeOutputModel:
+async def get_last_optimization(
+    repo: Annotated[ListRepository, Depends(get_optimizations_repository)],
+) -> OptimizeOutputModel:
 
-    try:
-        result = db.optimizations[-1]
-        return result
-    except IndexError as error:
+    result = repo.get_latest()
+
+    if result is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="No previous entry in database was found",
-        ) from error
+        )
+    return result
 
 
 @router.get("/optimize/{db_id}", status_code=status.HTTP_200_OK, response_model=OptimizeOutputModel)
-async def get_optimization(db_id: int) -> OptimizeOutputModel:
-    result = get_db_entry_by_id(db.optimizations, db_id)
+async def get_optimization(
+    db_id: int,
+    repo: Annotated[ListRepository, Depends(get_optimizations_repository)],
+) -> OptimizeOutputModel:
+    result = repo.get(db_id)
 
     if result:
         return result
@@ -78,10 +86,13 @@ async def get_optimization(db_id: int) -> OptimizeOutputModel:
 
 
 @router.delete("/optimize/{db_id}", response_model=OptimizeOutputModel)
-async def delete_optimization(db_id: int) -> OptimizeOutputModel:
+async def delete_optimization(
+    db_id: int,
+    repo: Annotated[ListRepository, Depends(get_optimizations_repository)],
+) -> OptimizeOutputModel:
 
     try:
-        db.optimizations.pop(db_id - 1)
+        repo.delete(db_id)
         return Response(status_code=status.HTTP_204_NO_CONTENT)
     except IndexError as error:
         raise HTTPException(
