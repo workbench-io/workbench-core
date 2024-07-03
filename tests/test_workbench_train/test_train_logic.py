1c780aa33c633ffd3481c1c7f9c0ad390882af93
jean.dossantos <jeandsantos88@gmail.com>
2024-07-03 10:30:20 +0200

    Refactor train logic tests and add model file creation test

--- tests/test_workbench_train/test_train_logic.py
+++ tests/test_workbench_train/test_train_logic.py
@@ -1,6 +1,7 @@
 import shutil
 from pathlib import Path
 
+import joblib
 import pandas as pd
 
 from workbench_train.train_data import TrainData
@@ -27,11 +28,67 @@ class TestTrainLogic:
             train_settings.model.exporting.path = dir_models
 
             train = TrainLogic()
-
             result = train.run(train_data, train_settings)
 
             assert isinstance(result, bool)
             assert result
+
+        finally:
+            shutil.rmtree(tmp_path)
+
+    def test_run_should_create_a_model_file(
+        self,
+        train_data: TrainData,
+        train_settings: TrainSettings,
+        features_and_targets: tuple[pd.DataFrame, pd.DataFrame],
+        tmp_path: Path,
+    ):
+
+        try:
+            train_data.features, train_data.targets = features_and_targets
+
+            dir_models = tmp_path.joinpath("models")
+            dir_models.mkdir(exist_ok=True, parents=True)
+
+            train_settings.model.exporting.path = dir_models
+
+            train = TrainLogic()
+            train.run(train_data, train_settings)
+
+            assert len(list(dir_models.glob("*.pkl"))) == 1
+
+            file_path = list(dir_models.glob("*.pkl"))[0]
+            assert file_path.exists()
+
+            model = joblib.load(filename=file_path)
+            assert hasattr(model, "predict")
+
+        finally:
+            shutil.rmtree(tmp_path)
+
+    def test_run_should_create_a_model_file_with_a_predict_method(
+        self,
+        train_data: TrainData,
+        train_settings: TrainSettings,
+        features_and_targets: tuple[pd.DataFrame, pd.DataFrame],
+        tmp_path: Path,
+    ):
+
+        try:
+            train_data.features, train_data.targets = features_and_targets
+
+            dir_models = tmp_path.joinpath("models")
+            dir_models.mkdir(exist_ok=True, parents=True)
+
+            train_settings.model.exporting.path = dir_models
+
+            train = TrainLogic()
+            train.run(train_data, train_settings)
+
+            file_path = list(dir_models.glob("*.pkl"))[0]
+
+            model = joblib.load(filename=file_path)
+            assert hasattr(model, "predict")
 
         finally:
             shutil.rmtree(tmp_path)
